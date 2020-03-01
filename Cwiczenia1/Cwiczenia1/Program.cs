29365cead30c251fc008cce9f3002077ad8d2096
Jakub Rusinowicz <s17455@pjwstk.edu.pl>
2020-03-01 16:05:36 +0100

    HttpClient test

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cwiczenia1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(html);
+            }
+            Console.WriteLine("Koniec!");
+        }
+    }
+}
